d740495b9932f50662cedb241e052979e7ec7c3e
feriwnarta <feriwnarta26@gmail.com>
2024-03-07 13:55:59 +0700

    tambah path ingrediets/units

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,31 @@
 <?php
-
-use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+	
+	use Illuminate\Http\Request;
+	use Illuminate\Support\Facades\Route;
+	use Illuminate\Support\Facades\URL;
+	
+	/*
+	|--------------------------------------------------------------------------
+	| Web Routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register web routes for your application. These
+	| routes are loaded by the RouteServiceProvider and all of them will
+	| be assigned to the "web" middleware group. Make something great!
+	|
+	*/
+	
+	Route::get('/', function () {
+		return URL::temporarySignedRoute('unsubscribe', now()->addMinute(30), ['user' => 1]);
+	});
+	
+	Route::get('users/{user}/unsubscribe', function (Request $request) {
+		if (!$request->hasValidSignature()) {
+			abort(403);
+		}
+		
+		return 'Process Unsubscribe';
+		
+	})
+		->name('unsubscribe')
+		->middleware('signed');
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,11 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function testData()
+    {
+
+
+    }
+
     use CreatesApplication;
 }
